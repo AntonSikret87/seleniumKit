bc672c75b9893afc85b4c0ddd03648879c8643bf
Anton Ka <antonkarabeinikov@gmail.com>
2017-07-06 21:53:58 +0300

    aaa

--- src/test/java/com/kit/core/googleTestForHub/GoogleTest.java
+++ src/test/java/com/kit/core/googleTestForHub/GoogleTest.java
@@ -2,13 +2,21 @@ package com.kit.core.googleTestForHub;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import com.kit.pages.google.GoogleSearchPage;
+import com.kit.pages.google.GoogleSearchResultPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class GoogleTest {
 
@@ -20,7 +28,7 @@ public class GoogleTest {
     {
         DesiredCapabilities caps = new DesiredCapabilities();
         if(platform.equalsIgnoreCase("Windows")) {
-            caps.setPlatform(Platform.WIN8_1);
+            //caps.setPlatform(Platform.WIN8_1);
             caps.setBrowserName(browser);
         }
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
@@ -29,5 +37,14 @@ public class GoogleTest {
     @Test(description="Test Google")
     public void testGoogle() throws InterruptedException {
         driver.get("https://www.google.com.ua");
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("slow");
     }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        //close browser
+        driver.close();
+    }
+
 }
